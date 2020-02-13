2b9b9e92be45ea18b7c1507bd66429caf31a1006
macruhinda <markruhinda47@gmail.com>
2020-02-13 16:15:53 +0300

    projectOne

---
+++ app/src/main/java/com/example/tresnot/DisplayMessageActivity.java
@@ -0,0 +1,25 @@
+package com.example.tresnot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_display_message);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("MESSAGE");
+        TextView messageView =(TextView)findViewById(R.id.messageTextView);
+        messageView.setText(message);
+    }
+    public void onClose(View view) {
+        super.finish();
+
+    }
+}
--- app/src/main/java/com/example/tresnot/MainActivity.java
+++ app/src/main/java/com/example/tresnot/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tresnot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+
+        EditText message = (EditText)findViewById(R.id.message);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra("MESSAGE", message.getText().toString());
+        startActivity(intent);
+        message.setText("");
     }
 }
