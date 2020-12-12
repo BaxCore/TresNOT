8cbcf9bc553eab5906a7a1a7391e3e12012c5d07
macruhinda <markruhinda47@gmail.com>
2020-12-12 14:20:21 +0300

    projectOne

--- app/src/main/java/com/example/tresnot/testmehere.java
+++ app/src/main/java/com/example/tresnot/testmehere.java
@@ -19,25 +19,17 @@ public class testmehere extends AppCompatActivity {
     }
     public void alarmsetting(View view){
         EditText editText = findViewById(R.id.alarmtext);
-        //changing the entered  text to a string then to an integer
-        int z =Integer.parseInt(editText.getText().toString());
+         int z =Integer.parseInt(editText.getText().toString());
 
-        //Create the intent and call your receiver explicit intent
         Intent intent= new Intent(getApplicationContext(),MyReceiverBroad.class);
 
-        //creating the pending intent
         PendingIntent pendingIntent;
         pendingIntent=PendingIntent.getBroadcast(this.getApplicationContext(),0,intent,0);
 
-        //calling the alarm manager class to use the alarm also accessing the system service method to access services provided by
-        // the android system
+
         AlarmManager alarmManager=(AlarmManager)getSystemService(ALARM_SERVICE);
-
-        //setting the alarm time
-        alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis() + (z* 1000),pendingIntent);
-
-        //make a toast using a class Toast
-        Toast.makeText(this," Your Alarm is set in "+  z +"seconds",Toast.LENGTH_LONG).show();
+   alarmManager.set(AlarmManager.RTC_WAKEUP,System.currentTimeMillis() + (z* 1000),pendingIntent);
+    Toast.makeText(this," Your Alarm is set in "+  z +"seconds",Toast.LENGTH_LONG).show();
 
     }
 }
