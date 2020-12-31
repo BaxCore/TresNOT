978adb1c576a1bf93d5b1e0cf838a554dca095a8
Vicent Balitema <bvicxbax@gmail.com>
2020-12-31 16:07:04 +0300

    manu

--- app/src/main/java/com/example/tresnot/DbHelper.java
+++ app/src/main/java/com/example/tresnot/DbHelper.java
@@ -1,8 +1,16 @@
 package com.example.tresnot;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.core.content.ContextCompat;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class DbHelper extends SQLiteOpenHelper {
     private static final String DBname = "dbname";
@@ -24,6 +32,31 @@ public class DbHelper extends SQLiteOpenHelper {
 
         dataDb.execSQL("drop table if exists "+TBname);
         onCreate(dataDb);
+
+    }
+    public boolean adding(String plc){
+        SQLiteDatabase sqLiteDatabase= this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("places", plc);
+
+        sqLiteDatabase.insert(TBname, null, contentValues);
+        return true;
+
+    }
+
+    public ArrayList Viewing(){
+        SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
+
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        Cursor cursor = sqLiteDatabase.rawQuery("select * from "+TBname, null);
+        cursor.moveToFirst();
+
+        if(!cursor.isAfterLast()){
+            arrayList.add(cursor.getString(cursor.getColumnIndex("plc")));
+            cursor.moveToNext();
+        }
+        return arrayList;
 
     }
 
--- app/src/main/java/com/example/tresnot/SavePlace.java
+++ app/src/main/java/com/example/tresnot/SavePlace.java
@@ -3,9 +3,14 @@ package com.example.tresnot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SavePlace extends AppCompatActivity {
     EditText plcV;
@@ -13,6 +18,9 @@ public class SavePlace extends AppCompatActivity {
     ListView listplaces;
 
         DbHelper dbHelper;
+        ArrayList arrayList;
+        ArrayAdapter arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,31 @@ public class SavePlace extends AppCompatActivity {
         save = findViewById(R.id.addPlace);
         listplaces = findViewById(R.id.listplaces);
 
+        final DbHelper dbHelper= new DbHelper(SavePlace.this);
+        arrayList = dbHelper.Viewing();
+        arrayAdapter = new ArrayAdapter(SavePlace.this, android.R.layout.activity_list_item, arrayList);
+        listplaces.setAdapter(arrayAdapter);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newplace = plcV.getText().toString();
+                if(!newplace.isEmpty()){
+                    if(dbHelper.adding(newplace)){
+                    plcV.setText("");
+                    Toast.makeText(getApplicationContext(), "Place Added", Toast.LENGTH_LONG).show();
+                    arrayList.clear();
+                    arrayList.addAll(dbHelper.Viewing());
+                    arrayAdapter.notifyDataSetChanged();
+                    listplaces.invalidateViews();
+                    listplaces.refreshDrawableState();
+
+
+
+                    }
+                }
+            }
+        });
 
 
 
