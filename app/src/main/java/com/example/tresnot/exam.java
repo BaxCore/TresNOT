d7b7665a9d54486468b2cbcd5c7f7353775e0318
Vicent Balitema <bvicxbax@gmail.com>
2021-01-03 22:47:32 +0300

    Notification

--- app/src/main/java/com/example/tresnot/exam.java
+++ app/src/main/java/com/example/tresnot/exam.java
@@ -3,6 +3,10 @@ package com.example.tresnot;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +14,7 @@ import android.widget.Button;
 
 public class exam extends AppCompatActivity {
     Button not;
+    String mee="Gwe man can you see this";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +33,16 @@ public class exam extends AppCompatActivity {
                         .setAutoCancel(true);
 
                 Intent i = new Intent(exam.this, MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                i.putExtra("See that",mee);
 
+                PendingIntent pd = PendingIntent.getActivity(exam.this,
+                        0, i, PendingIntent.FLAG_UPDATE_CURRENT);
+                bb.setContentIntent(pd);
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                notificationManager.notify(0, bb.build());
 
             }
         });
